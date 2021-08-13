cd270aeb47878320ea3f75b494c00959b5b99397
MuhammadHusayn Olimjonov <olimjonov926@gmail.com>
2021-08-13 11:04:35 +0500

    server changed

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ import http from 'http'
 import { host, PORT } from './config.js'
 
 const server = http.createServer( (req, res) => {
-	res.write(
-		JSON.stringify([
-			{ user_id: 1, username: "Halil" },
-			{ user_id: 2, username: "Nodir" },
-			{ user_id: 3, username: "Diyor" },
-		])
-	)
-	return res.end()
+	if(req.url == 'api/data' && req.method == 'GET') {
+		res.write(
+			JSON.stringify([
+				{ user_id: 1, username: "Halil" },
+				{ user_id: 2, username: "Nodir" },
+				{ user_id: 3, username: "Diyor" },
+				])
+			)
+		return res.end()
+	} else {
+		return res.end('Not found!')
+	}
 } )
 
 
