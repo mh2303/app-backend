9aff21a1e90fa5fa16999f0d7b2ad4a566386235
MuhammadHusayn Olimjonov <olimjonov926@gmail.com>
2021-08-13 11:13:23 +0500

    backend finished

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import http from 'http'
 import { host, PORT } from './config.js'
 
 const server = http.createServer( (req, res) => {
-	if(req.url == 'api/data' && req.method == 'GET') {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000')
+	if(req.url === '/api/data' && req.method.toUpperCase() === 'GET') {
 		res.write(
 			JSON.stringify([
 				{ user_id: 1, username: "Halil" },
