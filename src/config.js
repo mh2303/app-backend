2dc972e7a5daad617403fba864daab08a7acf803
MuhammadHusayn Olimjonov <olimjonov926@gmail.com>
2021-08-13 10:47:37 +0500

    finished mini server

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const host = 'localhost'
+const PORT = process.env.PORT || 4500
+
+export {
+	host, PORT
+}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
 import http from 'http'
+import { host, PORT } from './config.js'
+
+const server = http.createServer( (req, res) => {
+	res.write(
+		JSON.stringify([
+			{ user_id: 1, username: "Halil" },
+			{ user_id: 2, username: "Nodir" },
+			{ user_id: 3, username: "Diyor" },
+		])
+	)
+	return res.end()
+} )
+
+
+server.listen( PORT,  () => console.log('*' + PORT))
